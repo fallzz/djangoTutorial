478df3e3cefb5bafa28904ba9bdf1a97fecea803
fallzz <33145141+fallzz@users.noreply.github.com>
2019-05-09 00:45:36 +0900

    Add files via upload
    view custom
    admin page  custom

--- practice/polls/models.py
+++ practice/polls/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 class Question(models.Model):       #Subclass of django.db.models.Model class
     question_text=models.CharField(max_length=200)
-    pub_date=models.DateField('date published')
+    pub_date=models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
